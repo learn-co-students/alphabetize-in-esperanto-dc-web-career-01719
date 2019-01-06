41d372fb6dac98ace5b35b413407d74317252b9b
jameseclement <jameseclement@gmail.com>
2019-01-06 17:13:05 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,11 +1,14 @@
 require 'pry'
 
-
 def alphabetize(arr)
 esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 arr.sort_by {|phrase|
+
 phrase.split('').map {|letter|
+
 esperanto_alphabet.index(letter)
+
 }
 }
+
 end
